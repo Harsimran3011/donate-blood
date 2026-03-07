db28a906c5f2866fd380ca1bedb11ea9b4928e55
Harsimran singh <singhharsimran5200@gmail.com>
2026-03-07 23:29:03 +0530

    Donor Search Done

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,27 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import { useEffect, useRef } from "react";
+import LocomotiveScroll from "locomotive-scroll";
+import "locomotive-scroll/dist/locomotive-scroll.css";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: scrollRef.current,
+      smooth: true,
+    });
+
+    return () => {
+      scroll.destroy();
+    };
+  }, []);
+
+  return (
+    <div data-scroll-container ref={scrollRef}>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
